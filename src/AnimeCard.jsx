601460dfe11a7a8c5a48dd8af5b41d00e1fcc235
Nilanjan Sengupta <nilanjansengupta471@gmail.com>
2023-04-23 04:53:22 +0530

    ADD video-autoplay on hover feature

--- src/AnimeCard.jsx
+++ src/AnimeCard.jsx
@@ -1,17 +1,38 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const AnimeCard = ({ anime }) => {
+  const [isHovered, setIsHovered] = useState(false);
   const poster = (anime.images.jpg.image_url);
   const title = anime.title;
   const type = anime.type;
+  const embedUrl = anime.trailer?.embed_url;
+
   return (
-    <div className="anime" key={anime.mal_id}>
+    <div 
+      className="anime" 
+      key={anime.mal_id} 
+      onMouseEnter={() => setIsHovered(true)} 
+      onMouseLeave={() => setIsHovered(false)}
+    >
       <div>
         <p>{anime.year}</p>
       </div>
 
-      <div>
-        <img src={ poster !== "N/A" ? poster : "https://via.placeholder.com/400"} alt= {title} />
+      <div className="anime__media">
+        {isHovered && embedUrl ? (
+          <iframe 
+            src={embedUrl} 
+            frameBorder="0" 
+            allow="autoplay; encrypted-media" 
+            allowFullScreen
+            style={{
+              opacity:"1"
+            }}
+            title={title}
+          ></iframe>
+        ) : (
+          <img src={ poster !== "N/A" ? poster : "https://via.placeholder.com/400"} alt= {title} />
+        )}
       </div>
 
       <div>
